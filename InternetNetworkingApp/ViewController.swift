8c34e14d89b7571c06bf380676c5af828fbc0320
tporterz <gamingintensifys@gmail.com>
2020-01-08 07:47:32 -0700

    Initial Commit

---
+++ InternetNetworkingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InternetNetworkingApp
+//
+//  Created by Porter, Tyler on 1/8/20.
+//  Copyright © 2020 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
